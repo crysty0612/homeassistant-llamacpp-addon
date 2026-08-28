7828723e12688579704d6ed1286291a3f0217f87
Cristi C <cristian.cimpean@plentymarkets.de>
2026-08-28 17:53:47 +0300

    fix: match versioned libraries in LD_LIBRARY_PATH discovery

--- llamacpp/run.sh
+++ llamacpp/run.sh
@@ -93,7 +93,7 @@ if [ "$USE_SYCL" = "true" ]; then
     # setvars.sh may not be present in minimal runtime installs, so we manually
     # find and append all Intel library directories to ensure libsvml.so is found
     if [ -d "/opt/intel" ]; then
-        INTEL_LIBS=$(find /opt/intel -name "*.so" -exec dirname {} \; | sort -u | tr '\n' ':')
+        INTEL_LIBS=$(find /opt/intel -name "*.so*" -exec dirname {} \; | sort -u | tr '\n' ':')
         export LD_LIBRARY_PATH="$INTEL_LIBS$LD_LIBRARY_PATH"
     fi
 elif [ "$USE_VULKAN" = "true" ]; then
